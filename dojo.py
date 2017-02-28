fef9df9538a96878cb65407f97e0e073d4e78e8e
oscamaina <mainaoscar40@gmail.com>
2017-02-28 19:00:05 +0300

    Initial project functions layout

---
+++ dojo.py
@@ -0,0 +1,12 @@
+class Dojo():
+
+	def __init__(self):
+		pass
+
+	def create_room(self, room_name, room_type):
+		""" function to create a unique room space """
+		pass
+		
+	def add_person(self, name, category, accomodation="N"):
+		""" function to add a person """
+		pass
