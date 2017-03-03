9ad452a4e74346a41ba03362d1bd8504e0e0ffbb
oscamaina <mainaoscar40@gmail.com>
2017-03-03 08:26:17 +0300

    remodelled method create_room
    remodelled create_room method to use rooms model

--- dojo.py
+++ dojo.py
@@ -1,32 +1,36 @@
+from rooms import Office, LivingSpace
+from person import Fellow, Staff
+
 class Dojo():
 
 	def __init__(self):
 		self.rooms = []
 		self.offices = []
 		self.livingSpaces = []
+		self.persons = []
 		self.fellows = []
 		self.staffs = []
 
-	def create_room(self, room_name):
+	def create_room(self, room_type, room_name):
 		""" function to create a unique room space """
 		room_name = room_name.lower()
+		room_type = room_type.lower()
 
 		if room_name in self.rooms:
 			return "Room already exists"
 		else:
-			room_type = input("Enter room type for " + room_name + ":")
-			room_type = room_type.lower()
 			if room_type in ('office', 'living'):
-				self.rooms.append({'room_name': room_name,'room_type': room_type})
+				self.rooms.append(room_name)
 				if room_type == 'office':
-					self.offices.append({'room_name': room_name,'room_type': room_type})
+					room = Office(room_name)
+					self.offices.append(room)
 					return "Office added successfully"
 				elif room_type == 'living':
-					self.livingSpaces.append({'room_name': room_name,'room_type': room_type})
+					room = LivingSpace(room_name)
+					self.livingSpaces.append(room)
 					return "Living space added successfully"
 			else:
 				return "Wrong room type"
-		
 	def add_person(self, name, category, accomodation="N"):
 		""" function to add a person """
 		pass
