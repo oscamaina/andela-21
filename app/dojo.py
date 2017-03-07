acfcab86ed1a450c9511886713ab218b5ddbd605
oscamaina <mainaoscar40@gmail.com>
2017-03-08 00:32:06 +0300

    updates file
    removed unnecessary code, updated the allocate function to append either
    intance of fellow or staff

--- app/dojo.py
+++ app/dojo.py
@@ -8,7 +8,7 @@ class Dojo():
 	def __init__(self):
 		self.all_rooms = []
 		self.offices = []
-		self.livingSpaces = []
+		self.living_spaces = []
 		self.all_people = []
 		self.fellows = []
 		self.staffs = []
@@ -27,59 +27,56 @@ class Dojo():
 			if found:
 				msg += "Room with name " + room_name + " already exists \n"
 			else:
-				if room_type in ('office', 'living'):
-					room = Room(room_name)
-					self.all_rooms.append(room)
 				if room_type == 'office':
 					room = Office(room_name)
 					self.offices.append(room)
+					self.all_rooms.append(room)
 					msg += "Office " + room_name + " added successfully \n"
 				elif room_type == 'living':
 					room = LivingSpace(room_name)
-					self.livingSpaces.append(room)
+					self.living_spaces.append(room)
+					self.all_rooms.append(room)
 					msg += "Living space " + room_name + " added successfully\n"
 				else:
 					msg += "Wrong room type \n"
-			return msg
+		return msg
 
 
 	def add_person(self, first_name, last_name, category, accomodation="N"):
 		""" function to add a person """
-		full_name = first_name.capitalize() + " " + last_name.capitalize()
-		category = category.lower()
-		accomodation = accomodation.upper()
 
-		if category in ("fellow", "staff"):
+		if isinstance(first_name, str) and isinstance(last_name, str):
+			if category.lower() == "fellow":
+				person = Fellow(first_name, last_name, accomodation)
+				person.id = int(len(self.all_people)+1)
+				self.fellows.append(person)
 
-			id = len(self.all_people) + 1
-			person = Person(first_name, last_name)
-			self.all_people.append(person)
-		if category == "fellow":
-			person = Fellow(first_name, last_name, accomodation)
-			self.fellows.append(person)
-			if accomodation == 'Y':
-				return "Fellow added successfully with id " + str(id) + ". " \
-				+ self.allocate_office(full_name) + ". " + \
-				self.allocate_living(full_name)
-			return "Fellow added successfully with id " + str(id) + ". " \
-			+ self.allocate_office(full_name)
+				self.all_people.append(person)
+				if accomodation.upper() == 'Y':
+					return "Fellow " + person.first_name + " " + \
+					 person.last_name + " added successfully with id " \
+					 + str(person.id) + ", " + self.allocate_office(person) \
+					 + " and " + self.allocate_living(person)
+				return "Fellow " + person.first_name + " " + person.last_name\
+				 + " added successfully with id " + str(person.id) + " and " \
+				+ self.allocate_office(person)
 
-		elif category == "staff":
-			person = Staff(first_name, last_name)
-			if accomodation == "Y":
-				return "Sorry! staff can't be accomodated"
+			elif category.lower() == "staff":
+				if accomodation.upper() == "Y":
+					return "Sorry! staff can't be accomodated"
+				else:
+					person = Staff(first_name, last_name)
+					person.id = int(len(self.all_people)+1)
+					self.staffs.append(person)
+					self.all_people.append(person)
+					return "Staff " + person.first_name + " " + \
+					person.last_name + " added successfully with id " \
+					+ str(person.id) + " and " + self.allocate_office(person)
 			else:
-				self.staffs.append(person)
-				return "Staff added successfully with id " + str(id) + ". " \
-				+ self.allocate_office(full_name)
-		else:
-			return "Wrong category. Can only be fellow or staff"
+				return "Wrong category. Can only be fellow or staff"
+	def allocate_office(self, person):
+		""" Allocates office to person added """
 
-	def allocate_office(self, full_name):
-		"""
-		filters offices with space and
-		allocates office to person added
-		"""
 		office_with_space = []
 		for office_allocate in self.offices:
 			if len(office_allocate.occupants) < office_allocate.max_capacity:
@@ -88,30 +85,28 @@ class Dojo():
 		if len(office_with_space) > 0:
 			random_office = choice(office_with_space)
 			if len(random_office.occupants) < random_office.max_capacity:
-				random_office.occupants.append(full_name)
-				return "Allocated " + full_name + " to " + \
-				random_office.room_name + " Office space"
+				random_office.occupants.append(person)
+				return "allocated to " + random_office.room_name \
+				 + " Office space"
 			else:
 				return "No Living rooms with space"
 		else:
 			return "No available living rooms"
 
-	def allocate_living(self, full_name):
-		"""
-		    filters living rooms with space
-		    and allocates living space to fellow requested """
+	def allocate_living(self, person):
+		""" Allocates living space to fellow added """
 
 		living_with_space = []
-		for living_allocate in self.livingSpaces:
+		for living_allocate in self.living_spaces:
 			if len(living_allocate.occupants) < living_allocate.max_capacity:
 				living_with_space.append(living_allocate)
 
 		if len(living_with_space) > 0:
 			random_living_space = choice(living_with_space)
 			if len(random_living_space.occupants) < random_living_space.max_capacity:
-				random_living_space.occupants.append(full_name)
-				return "Allocated " + full_name + " to " + \
-				random_living_space.room_name + " Living space"
+				random_living_space.occupants.append(person)
+				return "allocated to " + random_living_space.room_name \
+				 + " Living space"
 			else:
 				return "No Living rooms with space"
 		else:
