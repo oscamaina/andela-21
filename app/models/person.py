4aafacad4cce7f9676763e80bde84f06c5a7f063
oscamaina <mainaoscar40@gmail.com>
2017-03-07 16:50:11 +0300

    renamed models to app

--- app/models/person.py
+++ app/models/person.py
@@ -1,21 +1,22 @@
 class Person(object):
     """ class person """
-    def __init__(self, name, category = 'none'):
+    def __init__(self, first_name, last_name, category = 'none'):
         """ initializing person attributes """
-        self.name = name
+        self.first_name = first_name
+        self.last_name = last_name
         self.category = category
 
 class Fellow(Person):
     """ class fellow inheriting class person """
-    def __init__(self, name, accomodation="N"):
+    def __init__(self, first_name, last_name, accomodation="N"):
         """ Initializing attributes of parent class """
-        super(Fellow, self).__init__(name)
+        super(Fellow, self).__init__(first_name, last_name)
         self.category = 'fellow'
         self.accomodation = accomodation
 
 class Staff(Person):
     """ class staff inheriting class person """
-    def __init__(self, name):
+    def __init__(self, first_name, last_name):
         """ Initializing attributes of parent class """
-        super(Staff, self).__init__(name)
+        super(Staff, self).__init__(first_name, last_name)
         self.category = 'staff'
