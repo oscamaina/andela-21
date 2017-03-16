5e073a0fbed8c220a8607626a62d4230221f9de8
oscamaina <mainaoscar40@gmail.com>
2017-03-16 11:31:05 +0300

    refactored tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -12,7 +12,8 @@ class TestDatabase(unittest.TestCase):
         self.dojo.create_room('living', ["kigali"])
         self.dojo.create_room('office', ["Django"])
         self.dojo.add_person("Maina", "oscar", "fellow")
-        output = self.dojo.save_state("flask.db")
+        self.dojo.add_person("Ken", "musomi", "staff")
+        output = self.dojo.save_state("somedb.db")
 
         #connect to db and create session
         engine = create_engine('sqlite:///database/somedb.db')
@@ -24,15 +25,14 @@ class TestDatabase(unittest.TestCase):
         persons = session.query(PersonModel).all()
 
         self.assertEqual(len(rooms),2)
-        self.assertEqual(len(persons),3)
-        self.assertEqual(output, "App data has been stored in somedb.db database")
+        self.assertEqual(len(persons),2)
+        self.assertEqual(output,"App data has been stored in somedb.db database")
 
     def test_successfully_load_state(self):
         """ Test loading data from database to application """
-        initial_room_count= len(self.dojo.all_rooms)
         self.dojo.load_state("flask.db")
-        room_count_after = len(self.dojo.all_rooms)
-        self.assertEqual(room_count_after - initial_room_count, 8)
+        self.assertEqual(len(self.dojo.all_rooms),8)
+        self.assertEqual(len(self.dojo.all_people),0)
 
     def test_load_state_with_non_existing_database(self):
         """Tests for loading state with a database that doesn't exist"""
