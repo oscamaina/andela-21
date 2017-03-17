2ee7378e6f063b1e0bd08fb71ed05ab69d2ce361
oscamaina <mainaoscar40@gmail.com>
2017-03-17 10:09:21 +0300

    refactored tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -6,22 +6,26 @@ from app.dojo import *
 class TestDatabase(unittest.TestCase):
     def setUp(self):
         self.dojo = Dojo()
-        self.fDojo = Dojo()
-        self.fDojo.create_room('living', ["kigali"])
-        self.fDojo.create_room('office', ["Django"])
-        self.fDojo.add_person("Maina", "oscar", "fellow")
-        self.fDojo.add_person("Ken", "musomi", "staff")
+        self.second_instance = Dojo()
+        self.second_instance.create_room('living', ["kigali"])
+        self.second_instance.create_room('office', ["Django"])
+        self.second_instance.add_person("Maina", "oscar", "fellow")
+        self.second_instance.add_person("Ken", "musomi", "staff")
 
     def test_successfully_saves_state(self):
         """ Test saves application state to Database. """
-        expected = self.dojo.save_state("somedb.db")
+        expected = self.second_instance.save_state("somedb.db")
         self.assertEqual(expected,"App data has been stored in somedb.db database")
 
     def test_successfully_load_state(self):
         """ Test loading data from database to application """
-        expected = self.fDojo.load_state("somedb.db")
-        self.assertEqual(len(self.fDojo.all_rooms),2)
-        self.assertEqual(len(self.fDojo.all_people),2)
+        self.second_instance.load_state("somedb.db")
+        self.assertEqual(len(self.second_instance.all_rooms),2)
+        self.assertEqual(len(self.second_instance.all_people),2)
+
+        self.dojo.load_state("somedb.db")
+        self.assertEqual(len(self.dojo.all_rooms),0)
+        self.assertEqual(len(self.dojo.all_people),0)
 
     def test_load_state_from_non_existing_database(self):
         """Tests for loading state from a database that doesn't exist"""
