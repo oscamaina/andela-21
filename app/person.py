6172a7893cda58da88ac4137c0b4ddc826e580d8
oscamaina <mainaoscar40@gmail.com>
2017-03-19 12:29:18 +0300

    Added attributes

--- app/person.py
+++ app/person.py
@@ -1,23 +1,24 @@
 class Person(object):
     """ class person """
-    def __init__(self, first_name, last_name, category = 'none', id =0):
+    def __init__(self, name, category = 'none'):
         """ initializing person attributes """
-        self.first_name = first_name
-        self.last_name = last_name
+        self.name = name
         self.category = category
-        self.id = id
+        self.office = ""
+        self.living = ""
+        self.person_id= ""
 
 class Fellow(Person):
     """ class fellow inheriting class person """
-    def __init__(self, first_name, last_name, accomodation="N"):
+    def __init__(self, name, accomodation="N"):
         """ Initializing attributes of parent class """
-        super(Fellow, self).__init__(first_name, last_name)
+        super(Fellow, self).__init__(name)
         self.category = 'fellow'
         self.accomodation = accomodation
 
 class Staff(Person):
     """ class staff inheriting class person """
-    def __init__(self, first_name, last_name):
+    def __init__(self, name):
         """ Initializing attributes of parent class """
-        super(Staff, self).__init__(first_name, last_name)
+        super(Staff, self).__init__(name)
         self.category = 'staff'
