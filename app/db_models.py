f8fa3d077e9f2f93ad338e2f655dcfbbcb0ab8ff
oscamaina <mainaoscar40@gmail.com>
2017-03-19 12:32:23 +0300

    Adds db models

---
+++ app/db_models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Boolean, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+base = declarative_base()
+
+class RoomModel(base):
+
+	__tablename__ = 'rooms'
+	room_id = Column(Integer, primary_key=True)
+	room_name = Column(String(50), unique=True, nullable=False)
+	room_type = Column(String(50), nullable=False)
+	max_capacity = Column(Integer(), nullable=False)
+	occupants = Column(String)
+
+
+class PersonModel(base):
+
+	__tablename__ = 'persons'
+	id = Column(Integer, primary_key = True)
+	person_id = Column(Integer)
+	name = Column(String(50), nullable=False)
+	category = Column (String(50), nullable=False)
+	office_allocated = Column(String, nullable=True)
+	wants_accomodation = Column(String)
+	living_allocated = Column(String, nullable=True)
